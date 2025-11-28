af90c6b97cf1e4f7cb2ffcaf9d600b46c3a9263e
NakoPCH <okaninho_fb@hotmail.com>
2025-11-29 00:42:05 +0200

    fix for not matching ports

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { connectDatabase } from "./config/database.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 const startServer = async () => {
   try {
